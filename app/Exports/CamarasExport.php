13e3365830cc67e15a7decba61e894347963ef3b
Exequiel Marichal <exemarichal@gmail.com>
2023-05-29 18:38:46 -0300

    cambios en la query

--- app/Exports/CamarasExport.php
+++ app/Exports/CamarasExport.php
@@ -12,7 +12,25 @@ class CamarasExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return Camara::select('id', 'nombre', 'sitio', 'inteligencia', 'marca', 'modelo', 'etapa', 'latitud', 'longitud', 'fecha_instalacion')->get();
+        //return Camara::select('id', 'nombre', 'sitio', 'inteligencia', 'marca', 'modelo', 'etapa', 'latitud', 'longitud', 'fecha_instalacion')->get();
+        $camaras = Camara::select(
+            'camaras.id',
+            'camaras.nombre',
+            'camaras.sitio',
+            'camaras.inteligencia',
+            'tipo_camara.tipo as tipo_camara',
+            'tipo_camara.marca as marca',
+            'tipo_camara.modelo as modelo',
+            'camaras.etapa',
+            'camaras.latitud',
+            'camaras.longitud',
+            'camaras.fecha_instalacion',
+            'destino.nombre as dependencia'
+        )
+        ->leftJoin('tipo_camara', 'camaras.tipo_camara_id', '=', 'tipo_camara.id')
+        ->leftJoin('destino', 'camaras.destino_id', '=', 'destino.id')
+        ->get();
+        return $camaras;
         //return Camara::all();
     }
 
@@ -23,12 +41,14 @@ class CamarasExport implements FromCollection, WithHeadings
             'Nombre',
             'Sitio',
             'Inteligencia',
+            'Tipo',
             'Marca',
             'Modelo',
             'Etapa',
             'Latitud',
             'Longitud',
             'Fecha de Instalación',
+            'Dependencia',
         ];
     }
 }

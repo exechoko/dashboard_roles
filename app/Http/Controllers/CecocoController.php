2e12799c29789e7f749840bb3e724dacbcfac416
Exequiel Marichal <exemarichal@gmail.com>
2023-11-09 14:53:14 -0300

    prueba

--- app/Http/Controllers/CecocoController.php
+++ app/Http/Controllers/CecocoController.php
@@ -147,7 +147,7 @@ class CecocoController extends Controller
     // Función para obtener la dirección a partir de las coordenadas
     private function getAddressGoogle($lat, $lng)
     {
-        $apiKey = 'AIzaSyDHCwd1Btzh0xFM2ksZKbkrmhDQ9uIHFvE'; // Reemplaza con tu clave de API de Google Maps
+        $apiKey = ''; // Reemplaza con tu clave de API de Google Maps
 
         // Realizar solicitud a la API de geocodificación
         $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$apiKey";

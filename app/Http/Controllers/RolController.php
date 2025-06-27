7a9ae1c0569d36c3724d19c0098b957bee203eef
Exequiel <exemarichal@gmail.com>
2025-06-27 16:02:31 -0300

    aumentar la paginación de roles en la vista de índice de 5 a 100

--- app/Http/Controllers/RolController.php
+++ app/Http/Controllers/RolController.php
@@ -24,7 +24,7 @@ class RolController extends Controller
      */
     public function index()
     {
-        $roles = Role::paginate(5);
+        $roles = Role::paginate(100);
         return view('roles.index', compact('roles'));
     }
 

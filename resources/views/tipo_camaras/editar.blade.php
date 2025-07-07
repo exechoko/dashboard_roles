dec27ade3ca336644425c35e39706731c63fcbd9
Exequiel <exemarichal@gmail.com>
2025-07-07 14:50:25 -0300

    Agregar selección de tipo de cámara y mejorar la carga de observaciones en las vistas de creación y edición

--- resources/views/tipo_camaras/editar.blade.php
+++ resources/views/tipo_camaras/editar.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#tipo').select2({
+                placeholder: "Seleccione un tipo de cámara",
+                allowClear: true
+            });
+        });
+    </script>
+@endpush
+
 @section('content')
     <section class="section">
         <div class="section-header">
@@ -23,16 +35,20 @@
                                 </div>
                             @endif
 
-                            <form action="{{ route('tipo-camara.update', $tipoCamara->id) }}" method="POST" enctype="multipart/form-data">
+                            <form action="{{ route('tipo-camara.update', $tipoCamara->id) }}" method="POST"
+                                enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
                                 <div class="row">
                                     <div class="col-xs-12 col-sm-12 col-md-12">
                                         <div class="form-group">
                                             <label for="tipo">Tipo</label>
-                                            <label class="alert alert-light ml-3" for="tipo">Fija - Fija FR - Fija LPR - Domo - Domo Dual - Múltiples canales</label>
-                                            <input type="text" name="tipo" class="form-control"
-                                                value="{{ $tipoCamara->tipo }}">
+                                            <select name="tipo" id="tipo" class="form-control">
+                                                <option value="">Seleccione un tipo</option>
+                                                @foreach($tipos as $tipo)
+                                                    <option value="{{ $tipo }}" {{ $tipoCamara->tipo == $tipo ? 'selected' : '' }}>{{ $tipo }}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="col-xs-12 col-sm-12 col-md-4">
@@ -60,21 +76,22 @@
                                         <div class="col-sm-4 col-md-6 pl-0 form-group">
                                             <label>Imagen:</label>
                                             <br>
-                                            <label for="imagen"  class="image__file-upload btn btn-primary text-white"
-                                                    tabindex="2"> Elegir
+                                            <label for="imagen" class="image__file-upload btn btn-primary text-white"
+                                                tabindex="2"> Elegir
                                                 <input type="file" name="imagen" id="imagen" class="d-none">
                                             </label>
                                         </div>
                                         <div class="col-sm-3 preview-image-video-container float-right mt-1">
                                             <img id='edit_preview_photo'
-                                                 class="img-thumbnail user-img user-profile-img profilePicture"
-                                                 src="{{asset( $tipoCamara->imagen )}}"/>
+                                                class="img-thumbnail user-img user-profile-img profilePicture"
+                                                src="{{asset($tipoCamara->imagen)}}" />
                                         </div>
                                     </div>
                                     <div class="col-xs-12 col-sm-12 col-md-12">
                                         <div class="form-floating">
                                             <label for="observaciones">Observaciones</label>
-                                            <textarea class="form-control" name="observaciones" style="height: 100px">{{ $tipoCamara->observaciones }}</textarea>
+                                            <textarea class="form-control" name="observaciones"
+                                                style="height: 100px">{{ $tipoCamara->observaciones }}</textarea>
                                         </div>
                                         <br>
                                         <button type="submit" class="btn btn-primary">Guardar</button>
